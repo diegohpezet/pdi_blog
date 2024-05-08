85edcbd7e500a2eb9d4f082c4dd9e9a6fcd1895b
diegohpezet <diegohpezet@gmail.com>
2024-05-07 23:44:30 -0300

    fix: media files path refference

--- utils/getFileContent.js
+++ utils/getFileContent.js
@@ -2,10 +2,10 @@ import fs, {readFileSync} from 'fs';
 import matter from 'gray-matter';
 
 export default function getFileContent (slug) {
-  const folder = "media/lessons/";
+  const folder = "/media/lessons/";
   const file = `${folder}${slug}`;
   console.log(file)
-  const content = readFileSync(file, 'utf-8');
+  const content = readFileSync(process.cwd() + file, 'utf-8');
   const matterResult = matter(content)
   return matterResult.content;
 };
