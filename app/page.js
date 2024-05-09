647c73bd9aa33a3ad16ea71519672bdc5f85bb72
diegohpezet <diegohpezet@gmail.com>
2024-05-09 15:46:37 -0300

    fix: replaced hello world

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello world</h1>
+      <h1>Proyecto, Diseño e Implementación de Sistemas Computacionales</h1>
+      <hr />
+      <p>Bienvenido al sitio de la materia PDISC de la EEST N°4.</p>
+      <h2>Docentes</h2>
+      <ul>
+        <li>Futryk Fabricio</li>
+        <li>Moscardini Victoria</li>
+        <li>Pezet Diego</li>
+      </ul>
     </main>
   );
 }
